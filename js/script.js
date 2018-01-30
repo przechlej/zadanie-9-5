35287761a56e8d75f7181004584aad48ce949af6
Piotr Przechlewski <haesor@gmail.com>
2018-01-30 17:24:42 +0100

    add index.html, script.js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var ButtonClass = document.getElementsByClassName("button");
+var i;
+
+function button () {
+for (var i = 0 ; i < ButtonClass.lenght; i++)
+}
+ButtonClass[i].innerText = text;
+console.log(ButtonClass)
